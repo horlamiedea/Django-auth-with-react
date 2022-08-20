2943173bad96eb0cbff66f8aad1eff7dd6340abf
horlamiedea <horlamiedea@gmail.com>
2022-08-20 17:44:40 +0100

    cors

--- backend/settings.py
+++ backend/settings.py
@@ -190,7 +190,16 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://ethnosfe.herokuapp.com',
+    'https://frontend-jctv854uc-horlamiedea.vercel.app'
+] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'https://ethnosfe.herokuapp.com',
+    'https://frontend-jctv854uc-horlamiedea.vercel.app'
+]
 
 
 
